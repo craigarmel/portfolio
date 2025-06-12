fe76bd14ef88c42b79b55166c77b494236c5169f
Craig Armel YOUAKOUA TCHOUSSOKNOU <armel@Craigs-MacBook-Air.local>
2025-06-12 19:21:18 +0200

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ const Portfolio = () => {
       <div className="max-w-6xl mx-auto px-6 py-4">
         <div className="flex justify-between items-center">
         <div className="text-2xl font-bold bg-gradient-to-r from-pink-400 to-purple-400 bg-clip-text text-transparent">
-          Craig Armel
+          <a href="#"><img src="favicon.ico" alt="" /></a>
         </div>
         <div className="hidden md:flex space-x-8">
           {['hero', 'about', 'experience', 'projects', 'skills', 'contact'].map((section) => (
